e7ce1a7254a72441464b8061563e84a46c722331
Olivia Zhao <o.zhao.0001@gmail.com>
2024-07-29 22:41:41 -0400

    Commit from o.zhao.0001@gmail.com

--- src/industry_analysis.py
+++ src/industry_analysis.py
@@ -54,11 +54,32 @@ def get_and_add_naics():
     sicNaicsDf = pd.DataFrame.from_records(dictList)
     mergedDf = pd.merge(layoffDataFullSimpl, sicNaicsDf, how="left", on=["sic"])
     mergedDf = mergedDf[colnamesFull]
-    print("cool")
-    # mergedDf.to_csv(r"%s\data\layoffDataFullSimpl.csv" % os.path.normpath(os.path.join(os.getcwd(), os.pardir)),
-    #                 index=False)
+    # print("cool")
+    mergedDf.to_csv(r"%s\data\layoffDataFullSimpl.csv" % os.path.normpath(os.path.join(os.getcwd(), os.pardir)),
+                    index=False)
+
+
+def clean_aiie(digits=3):
+    aiieDf = pd.read_csv(r"%s\data\aiie.csv" % os.path.normpath(os.path.join(os.getcwd(), os.pardir)))
+    aiieDf["NAICS 3 Digits"] = [float(x) // (10 ** (4 - digits)) for x in aiieDf["NAICS"]]
+    return aiieDf.groupby(by=["NAICS 3 Digits"])[["AIIE"]].mean().reset_index()
+
+
+def assemble_industry_dataset(digits=3):
+    aiieDf = clean_aiie(digits)
+    if digits == 2:
+        layoffDataFullSimpl["naics"] = [float(x) // 10 for x in layoffDataFullSimpl["naics"]]
+    layoffsDfCount = layoffDataFullSimpl.groupby(by=["naics"])[["Company"]].count().reset_index()
+    layoffsDfCompanySize = layoffDataFullSimpl.groupby(by=["naics"])[["Dollars Raised (mil)"]].mean().reset_index()
+    layoffsDf = pd.merge(layoffsDfCount, layoffsDfCompanySize, how="left", on="naics")
+    mergedDf = pd.merge(layoffsDf, aiieDf, how="left", left_on="naics", right_on="NAICS 3 Digits")
+    return mergedDf
 
 
 if "__main__" == __name__:
-    get_and_add_naics()
-
+    print(clean_aiie())
+    print(assemble_industry_dataset(2))
+    assemble_industry_dataset(3).to_csv(r"%s\data\industryExposure3Digit.csv" % os.path.normpath(
+                                       os.path.join(os.getcwd(), os.pardir)), index=False)
+    assemble_industry_dataset(2).to_csv(r"%s\data\industryExposure2Digit.csv" % os.path.normpath(
+        os.path.join(os.getcwd(), os.pardir)), index=False)

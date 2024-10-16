0892f0c1b02c1a0914e416aacf77a3a50f3df6ad
Olivia Zhao <o.zhao.0001@gmail.com>
2024-10-16 16:29:18 -0400

    Commit from o.zhao.0001@gmail.com

--- src/mlr_analysis.py
+++ src/mlr_analysis.py
@@ -15,15 +15,16 @@ from sklearn import preprocessing
 from sklearn.preprocessing import OrdinalEncoder
 from statistics import stdev
 
+# data preprocessing
 ind_var_setup = ["AI Mentioned", "Reason Mentioned 1", "Reason Mentioned 2", "Reason Mentioned 3", "Date of Layoff",
                  "Percentage", "Number Laid Off"]
-dep_var = ["Stock Return On Closest Trading Date Post-Announcement (t)"]
+dep_var = ["Stock Return On Closest Trading Date Post-Announcement (t)", "Stock Return 3 Days After t"]
 other_vars = list(set(colnamesFull) - set(ind_var_setup + dep_var))
 layoffDataFullSimpl.drop(other_vars, axis=1, inplace=True)
-layoffDataFullSimpl.dropna(inplace=True)
-print(layoffDataFullSimpl.columns)
+# print(layoffDataFullSimpl.columns)
 
-layoffDataFullSimpl["Log(Number Laid Off)"] = [log(x) for x in layoffDataFullSimpl["Number Laid Off"] if type(x) is float]
+layoffDataFullSimpl["Log(Number Laid Off)"] = [log(x) for x in layoffDataFullSimpl["Number Laid Off"] if
+                                               type(x) is float]
 layoffDataFullSimpl["Year"] = pd.DatetimeIndex(layoffDataFullSimpl["Date of Layoff"]).year
 colnamesFull.insert(7, "Log(Number Laid Off)")
 colnamesFull.append("Year")
@@ -34,13 +35,25 @@ missing_reasons = ["business realignment", "definite business realignment", "aut
 simplified_reasons = list(set(simplifierMap.values())) + missing_reasons
 for reason in simplified_reasons:
     layoffDataFullSimpl[str(reason)] = np.where((layoffDataFullSimpl["Reason Mentioned 1"] == reason) |
-                                                               (layoffDataFullSimpl["Reason Mentioned 2"] == reason) |
-                                                               (layoffDataFullSimpl["Reason Mentioned 3"] == reason),
-                                                               1, 0)
+                                                (layoffDataFullSimpl["Reason Mentioned 2"] == reason) |
+                                                (layoffDataFullSimpl["Reason Mentioned 3"] == reason),
+                                                1, 0)
 
-publicDf = layoffDataFullSimpl[layoffDataFullSimpl["Stock Return On Closest Trading Date Post-Announcement (t)"].notna()]
+layoffDataFullSimpl.drop(["Reason Mentioned 1", "Reason Mentioned 2", "Reason Mentioned 3"], axis=1, inplace=True)
+layoffDataFullSimpl["Post"] = np.where(layoffDataFullSimpl["Year"] >= 2023, 1, 0)
+layoffDataFullSimpl["Post*AI"] = layoffDataFullSimpl["Post"] * layoffDataFullSimpl["AI Mentioned"]
+layoffDataFullSimpl["Post*Percentage"] = layoffDataFullSimpl["Post"] * layoffDataFullSimpl["Percentage"]
+
+publicDf = layoffDataFullSimpl.dropna()
+
+# check if there are any NA values left
+print(publicDf[publicDf.isna().any(axis=1)])
+# ------
+
+# first model
 ind_var = ["AI Mentioned", "Year_code", "Percentage", "Log(Number Laid Off)"] + simplified_reasons
 xDf = publicDf[ind_var]
+dep_var.remove("Stock Return 3 Days After t")
 yDf = publicDf[dep_var]
 
 x_train, x_test, y_train, y_test = train_test_split(xDf, yDf, test_size=.3, random_state=101)
@@ -49,22 +62,21 @@ model.fit(x_train, y_train)
 predictions = model.predict(x_test)
 
 importance = list(model.coef_)[0]
-for i,v in enumerate(importance):
-    print(str(ind_var[i]) + ": " + str(round(v, 4)))
+metrics = ["Mean squared error: " + str(mean_squared_error(y_test, predictions)),
+           "Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)),
+           "Mean absolute error: " + str(mean_absolute_error(y_test, predictions)),
+           "R2 Score: " + str(r2_score(y_test, predictions))]
+f = open(r"%s\docs\mlr_analysis_results.txt" % os.path.normpath(os.path.join(os.getcwd(), os.pardir)), "w")
+f.write("1st Model:\n")
+for i, v in enumerate(importance):
+    f.write(str(ind_var[i]) + ": " + str(round(v, 4)))
+    f.write("\n")
+f.writelines("\n".join(metrics))
 
-print("Mean squared error: " + str(mean_squared_error(y_test, predictions)))
-print("Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)))
-print("Mean absolute error: " + str(mean_absolute_error(y_test, predictions)))
-print("R2 score: " + str(r2_score(y_test, predictions)))
-
-print("\nMODEL SEPARATOR\n")
 # second model
-layoffDataFullSimpl["Post"] = np.where(layoffDataFullSimpl["Year"] >= 2023, 1, 0)
-layoffDataFullSimpl["Post*AI"] = layoffDataFullSimpl["Post"] * layoffDataFullSimpl["AI Mentioned"]
-layoffDataFullSimpl["Post*Percentage"] = layoffDataFullSimpl["Post"] * layoffDataFullSimpl["Percentage"]
-publicDf = layoffDataFullSimpl[layoffDataFullSimpl["Stock Return On Closest Trading Date Post-Announcement (t)"].notna()]
-ind_var = ["AI Mentioned", "Post", "Percentage", "Log(Number Laid Off)"] + simplified_reasons
-xDf = publicDf[ind_var]
+ind_var2 = ["AI Mentioned", "Post", "Post*AI", "Post*Percentage", "Percentage",
+            "Log(Number Laid Off)"] + simplified_reasons
+xDf = publicDf[ind_var2]
 yDf = publicDf[dep_var]
 
 x_train, x_test, y_train, y_test = train_test_split(xDf, yDf, test_size=.3, random_state=101)
@@ -73,12 +85,58 @@ model2.fit(x_train, y_train)
 predictions = model2.predict(x_test)
 
 importance = list(model2.coef_)[0]
-for i,v in enumerate(importance):
-    print(str(ind_var[i]) + ": " + str(round(v, 4)))
+metrics = ["Mean squared error: " + str(mean_squared_error(y_test, predictions)),
+           "Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)),
+           "Mean absolute error: " + str(mean_absolute_error(y_test, predictions)),
+           "R2 Score: " + str(r2_score(y_test, predictions))]
+f.write("\n\n2nd Model:\n")
+for i, v in enumerate(importance):
+    f.write(str(ind_var2[i]) + ": " + str(round(v, 4)))
+    f.write("\n")
+f.writelines("\n".join(metrics))
+f.write("\n\nStandard deviation: " + str(stdev(publicDf[dep_var[0]].tolist())))
 
-print("Mean squared error: " + str(mean_squared_error(y_test, predictions)))
-print("Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)))
-print("Mean absolute error: " + str(mean_absolute_error(y_test, predictions)))
-print("R2 score: " + str(r2_score(y_test, predictions)))
+# third model: Predicting stock return for day t + 3
+dep_var.append("Stock Return 3 Days After t")
+dep_var.remove("Stock Return On Closest Trading Date Post-Announcement (t)")
+xDf = publicDf[ind_var]
+yDf = publicDf[dep_var]
 
-print("\nStandard deviation: " + str(stdev(publicDf[dep_var[0]].tolist())))
+x_train, x_test, y_train, y_test = train_test_split(xDf, yDf, test_size=.3, random_state=101)
+model3 = LinearRegression()
+model3.fit(x_train, y_train)
+predictions = model3.predict(x_test)
+
+importance = list(model3.coef_)[0]
+metrics = ["Mean squared error: " + str(mean_squared_error(y_test, predictions)),
+           "Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)),
+           "Mean absolute error: " + str(mean_absolute_error(y_test, predictions)),
+           "R2 Score: " + str(r2_score(y_test, predictions))]
+f.write("\n\n3rd Model:\n")
+for i, v in enumerate(importance):
+    f.write(str(ind_var[i]) + ": " + str(round(v, 4)))
+    f.write("\n")
+f.writelines("\n".join(metrics))
+
+# fourth model: also predicting stock return for day t+3
+xDf = publicDf[ind_var2]
+x_train, x_test, y_train, y_test = train_test_split(xDf, yDf, test_size=.3, random_state=101)
+model4 = LinearRegression()
+model4.fit(x_train, y_train)
+predictions = model4.predict(x_test)
+
+importance = list(model4.coef_)[0]
+metrics = ["Mean squared error: " + str(mean_squared_error(y_test, predictions)),
+           "Root mean squared error: " + str(root_mean_squared_error(y_test, predictions)),
+           "Mean absolute error: " + str(mean_absolute_error(y_test, predictions)),
+           "R2 Score: " + str(r2_score(y_test, predictions))]
+f.write("\n\n4th Model:\n")
+for i, v in enumerate(importance):
+    f.write(str(ind_var2[i]) + ": " + str(round(v, 4)))
+    f.write("\n")
+f.writelines("\n".join(metrics))
+f.write("\n\nStandard deviation: " + str(stdev(publicDf[dep_var[0]].tolist())))
+f.close()
+
+descriptionDf = xDf.describe(include="all")
+descriptionDf.to_csv(r"%s\data\mlrAnalysisDescStats.csv" % os.path.normpath(os.path.join(os.getcwd(), os.pardir)))
